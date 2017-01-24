14afc48b2641337566f0bec61a1f7a41ea851906
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-01-24 08:54:32 -0700

    Item model and factory

---
+++ spec/models/item_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe Item do
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:description) }
+    it { is_expected.to validate_presence_of(:unit_price_in_cents) }
+    it { is_expected.to validate_presence_of(:merchant_id) }
+    it { is_expected.to validate_presence_of(:updated_at) }
+    it { is_expected.to validate_presence_of(:created_at) }
+  end
+end
