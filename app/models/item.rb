2eb658e4e282a650ceb6707fc73a204537d009d2
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-01-24 12:09:33 -0700

    Change validations to one line

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,6 @@
 class Item < ApplicationRecord
-  validates :name, presence: true
-  validates :description, presence: true
-  validates :merchant_id, presence: true
-  validates :unit_price_in_cents, presence: true
-  validates :updated_at, presence: true
-  validates :created_at, presence: true
+  validates :name, :description, :merchant_id, :unit_price_in_cents,
+    :updated_at, :created_at, presence: true
 
   belongs_to :merchant
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,7 +1,5 @@
 class Merchant < ApplicationRecord
-  validates :name, presence: true
-  validates :updated_at, presence: true
-  validates :created_at, presence: true
+  validates :name, :updated_at, :created_at, presence: true
 
   has_many :items
 end
