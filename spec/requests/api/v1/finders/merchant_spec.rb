07aaed2fe10506467622a6717d29818154a7afa7
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-01-24 15:17:39 -0700

    Refactor test merchants into let block

--- spec/requests/api/v1/finders/merchant_spec.rb
+++ spec/requests/api/v1/finders/merchant_spec.rb
@@ -5,63 +5,63 @@ describe 'Merchants API' do
   let(:date) {"2012-03-27T14:54:02.000Z"}
 
   context 'find by' do
+
+    let(:merchant_1) { create(:merchant) }
+    let(:merchant_2) { create(:merchant) }
+    let(:merchant_3) { create(:merchant) }
+
     it 'id' do
-      merchant1, merchant2, merchant3 = create_list(:merchant, 3)
-      id = merchant3.id
+      id = merchant_3.id
 
       get '/api/v1/merchants/find', params: {id: id}
       merchant = JSON.parse(response.body)
 
       expect(response).to be_success
-      expect(merchant['name']).to eq merchant3.name
+      expect(merchant['name']).to eq merchant_3.name
     end
 
     it 'name' do
-      merchant1, merchant2, merchant3 = create_list(:merchant, 3)
-      name = merchant3.name
+      name = merchant_3.name
 
       get '/api/v1/merchants/find', params: {name: name}
       merchant = JSON.parse(response.body)
 
       expect(response).to be_success
-      expect(merchant['id']).to eq merchant3.id
+      expect(merchant['id']).to eq merchant_3.id
     end
 
     it 'updated at' do
-      merchant1, merchant2, merchant3 = create_list(:merchant, 3)
-      merchant3.update(updated_at: date)
+      merchant_3.update(updated_at: date)
 
       get '/api/v1/merchants/find', params: {updated_at: date}
       merchant = JSON.parse(response.body)
 
       expect(response).to be_success
-      expect(merchant['id']).to eq merchant3.id
+      expect(merchant['id']).to eq merchant_3.id
     end
 
     it 'created at' do
-      merchant1, merchant2, merchant3 = create_list(:merchant, 3)
-      merchant3.update(created_at: date)
+      merchant_3.update(created_at: date)
 
       get '/api/v1/merchants/find', params: {created_at: date}
       merchant = JSON.parse(response.body)
 
       expect(response).to be_success
-      expect(merchant['id']).to eq merchant3.id
+      expect(merchant['id']).to eq merchant_3.id
     end
 
     it 'multiple attributes' do
-      merchant1, merchant2, merchant3 = create_list(:merchant, 3)
-      name = merchant3.name
-      merchant3.update(created_at: date)
-      merchant3.update(updated_at: date)
-      id = merchant3.id
+      name = merchant_3.name
+      merchant_3.update(created_at: date)
+      merchant_3.update(updated_at: date)
+      id = merchant_3.id
 
 
       get '/api/v1/merchants/find', params: {name: name, id: id, created_at: date, updated_at: date}
       merchant = JSON.parse(response.body)
 
       expect(response).to be_success
-      expect(merchant['id']).to eq merchant3.id
+      expect(merchant['id']).to eq merchant_3.id
     end
   end
 
