ffc77199e42dca8b2c6ac026cde96bcf0a70995a
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-01-24 16:55:02 -0700

    Fix transaction importer for credit card numbers

--- db/migrate/20170124231827_create_transactions.rb
+++ db/migrate/20170124231827_create_transactions.rb
@@ -2,7 +2,7 @@ class CreateTransactions < ActiveRecord::Migration[5.0]
   def change
     create_table :transactions do |t|
       t.references :invoice, foreign_key: true
-      t.integer :credit_card_number
+      t.text :credit_card_number
       t.integer :result
       t.datetime :credit_card_expiration_date
 
