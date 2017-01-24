28be5004d8f10d72913117cfe72fa31b26841e47
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-01-24 11:29:29 -0700

    Failing spec for InvoicesImporter

---
+++ spec/importers/invoices_importer_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe "Invoices importer" do
+  let(:bad) { './spec/fixtures/bad/invoices.csv' }
+  let(:good) { './spec/fixtures/good/invoices.csv' }
+
+  context "unsuccessful import" do
+    it "raises an error and does not save any invoices to the database" do
+      expect{InvoicesImporter.new(bad).import}
+        .to raise_error(ActiveRecord::RecordInvalid)
+
+      expect(Invoice.count).to eq 0
+    end
+  end
+
+  context "successful import" do
+    it "imports all the invoices and saves them to the database" do
+      expect{InvoicesImporter.new(good).import}
+        .to change{Invoice.count}
+        .by(8)
+    end
+  end
+end
