b01cf96c52d56cde42d9fb53343d42ce2c40ed81
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-01-23 19:41:18 -0700

    Merchants importer file

---
+++ app/importers/merchants_importer.rb
@@ -0,0 +1,35 @@
+require 'csv'
+
+class MerchantsImporter
+  attr_reader :filename,
+              :number_of_lines
+
+  def initialize(filename)
+    @filename = filename
+    @number_of_lines = `wc -l #{filename}`.to_i
+  end
+
+  def import
+    bar = ProgressBar.create(title: "Merchants", total: number_of_lines)
+
+    CSV.foreach(filename, headers: true) do |row|
+
+      merchant_attributes = {
+        name:       row['name'],
+        updated_at: to_date(row['updated_at']),
+        created_at: to_date(row['created_at'])
+      }
+            
+      Merchant.create!(merchant_attributes)
+
+      bar.increment
+    end
+    bar.finish
+  end
+
+  private
+
+  def to_date(row)
+    DateTime.parse(row)
+  end
+end
