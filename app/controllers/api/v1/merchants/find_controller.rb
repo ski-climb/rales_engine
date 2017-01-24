571b79444459f29df96cf11180b1fc5c8b4eed90
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-01-24 15:03:21 -0700

    Merchant find and find all

--- app/controllers/api/v1/merchants/find_controller.rb
+++ app/controllers/api/v1/merchants/find_controller.rb
@@ -1,13 +1,17 @@
 class API::V1::Merchants::FindController < ApplicationController
 
   def show
-    render json: Merchant.find_by(find_params)
+    render json: Merchant.find_by(search_params)
+  end
+
+  def index
+    render json: Merchant.where(search_params)
   end
 
   private
-
-  def find_params
-    params.permit(:id)
+  
+  def search_params
+    params.permit(:id, :name, :updated_at, :created_at)
   end
 
 end
