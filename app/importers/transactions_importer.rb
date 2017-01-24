d554c9d655596ecf862a05917e4b627cefebd56a
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-01-24 16:42:50 -0700

    Transaction importer test

---
+++ app/importers/transactions_importer.rb
@@ -0,0 +1,39 @@
+require 'csv'
+
+class TransactionsImporter
+  attr_reader :filename,
+              :number_of_lines
+
+  def initialize(filename)
+    @filename = filename
+    @number_of_lines = `wc -l #{filename}`.to_i
+  end
+
+  def import
+    bar = ProgressBar.create(title: "Transactions", total: number_of_lines)
+
+    CSV.foreach(filename, headers: true) do |row|
+
+      transaction_attributes = {
+        id:                 row['id'].to_i,
+        credit_card_number: row['name'].to_i,
+        invoice_id:         row['invoice_id'].to_i,
+        result:             row['result'],
+        updated_at:         to_date(row['updated_at']),
+        created_at:         to_date(row['created_at'])
+      }
+            
+      Transaction.create!(transaction_attributes)
+
+      bar.increment
+    end
+    bar.finish
+    puts "#{number_of_lines} transactions imported"
+  end
+
+  private
+
+  def to_date(row)
+    DateTime.parse(row)
+  end
+end
