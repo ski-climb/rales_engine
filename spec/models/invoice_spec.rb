5d149594130879a9be08881c33df979350b8be82
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-01-24 21:26:38 -0700

    Fixture files for InvoiceItem importer spec

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -11,6 +11,8 @@ describe Invoice do
     it { is_expected.to belong_to(:customer) }
     it { is_expected.to belong_to(:merchant) }
     it { is_expected.to have_many(:transactions) }
+    it { is_expected.to have_many(:invoice_items) }
+    it { is_expected.to have_many(:items).through(:invoice_items) }
   end
 
   describe "status" do
