9dfe3bfba6162a5c3d6104fbe67d7a5d3e7e1cb5
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-01-24 21:44:44 -0700

    Transaction relationship with invoice

--- spec/requests/api/v1/relationships/transaction_spec.rb
+++ spec/requests/api/v1/relationships/transaction_spec.rb
@@ -7,13 +7,12 @@ describe 'Transactions API relationship' do
     invoice = transaction.invoice
 
     get "/api/v1/transactions/#{transaction.id}/invoice"
-    invoice = JSON.parse(response.body)
+    response_invoice = JSON.parse(response.body)
 
     expect(response).to be_success
-    expect(invoice).to be_a Hash
-    expect(invoice_id).to eq invoice['id']
-    expect(invoice.merchant_id).to eq invoice['merchant_id']
-    expect(invoice.customer).to eq invoice['customer_id']
-    expect(invoice.keys.count).to eq 3
+    expect(response_invoice).to be_a Hash
+    expect(response_invoice['id']).to eq invoice.id
+    expect(response_invoice['merchant_id']).to eq invoice.merchant_id
+    expect(response_invoice['customer_id']).to eq invoice.customer_id
   end
 end
