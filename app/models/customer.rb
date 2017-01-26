63c43a93b1777f565fb65cab1a9b3d07cc5bc81a
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-01-26 13:43:55 -0700

    Customer has many invoice items

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,6 +4,7 @@ class Customer < ApplicationRecord
   has_many :invoices
   has_many :transactions, through: :invoices
   has_many :merchants, through: :invoices
+  has_many :invoice_items, through: :invoices
 
   def favorite_merchant
     merchants
