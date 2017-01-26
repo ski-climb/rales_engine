f74d3d04609784cdc7b6d513affeb4636a441b7b
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-01-26 11:50:44 -0700

    Add unsuccessful transactions test

--- spec/requests/api/v1/business-intelligence/merchants_most_items_spec.rb
+++ spec/requests/api/v1/business-intelligence/merchants_most_items_spec.rb
@@ -1,30 +1,49 @@
 require 'rails_helper'
 
 describe 'Merchants API most items' do
-  it 'returns top merchants ranked by total items sold' do
-    quantity = 2
+  context 'multiple invoice items' do
+    it 'returns merchants ranked by items sold' do
+      merchant_1, merchant_2, merchant_3 = create_list(:merchant, 3)
 
-    first_merchant = create(:merchant)
-    second_merchant = create(:merchant)
-    third_merchant = create(:merchant)
+      merchant_1_invoice = create(:invoice, merchant: merchant_1)
+      merchant_2_invoice = create(:invoice, merchant: merchant_2)
+      merchant_3_invoice = create(:invoice, merchant: merchant_3)
 
-    first_merchant_invoice = create(:invoice, merchant: first_merchant)
-    second_merchant_invoice = create(:invoice, merchant: second_merchant)
-    third_merchant_invoice = create(:invoice, merchant: third_merchant)
+      transaction = create(:transaction, invoice: merchant_1_invoice)
+      transaction = create(:transaction, invoice: merchant_2_invoice)
+      transaction = create(:transaction, invoice: merchant_3_invoice)
 
-    create_list(:invoice_item, 3, quantity: 10, invoice: first_merchant_invoice)
-    create_list(:invoice_item, 3, quantity: 8, invoice: second_merchant_invoice)
-    create_list(:invoice_item, 3, quantity: 5, invoice: third_merchant_invoice)
+      create_list(:invoice_item, 3, quantity: 10, invoice: merchant_1_invoice)
+      create_list(:invoice_item, 5, quantity: 5, invoice: merchant_2_invoice)
+      create_list(:invoice_item, 7, quantity: 3, invoice: merchant_3_invoice)
 
-    get "/api/v1/merchants/most_items?quantity=#{quantity}"
-    merchants = JSON.parse(response.body)
-    response_merchant_1 = merchants.first
-    response_merchant_2 = merchants.last
+      get "/api/v1/merchants/most_items?quantity=2"
+      merchants = JSON.parse(response.body)
+      response_merchant_1 = merchants.first
+      response_merchant_2 = merchants.last
 
-    expect(response).to be_success
-    expect(merchants).to be_a Array
-    expect(merchants.count).to eq 2
-    expect(response_merchant_1['id']).to eq first_merchant.id
-    expect(response_merchant_2['id']).to eq second_merchant.id
+      expect(response).to be_success
+      expect(merchants).to be_a Array
+      expect(merchants.count).to eq 2
+      expect(response_merchant_1['id']).to eq merchant_1.id
+      expect(response_merchant_2['id']).to eq merchant_2.id
+    end
+  end
+
+  context 'unsuccessful transaction' do
+    it 'does not return merchant' do
+      merchant = create(:merchant)
+
+      merchant_invoice = create(:invoice, merchant: merchant)
+      transaction = create(:transaction, invoice: merchant_invoice, result: 'failed')
+      create(:invoice_item, invoice: merchant_invoice)
+
+      get "/api/v1/merchants/most_items?quantity=2"
+      merchants = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(merchants).to be_a Array
+      expect(merchants.count).to eq 0
+    end
   end
 end
